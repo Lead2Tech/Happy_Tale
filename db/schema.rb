64cb537cb7600b89507615c59e651d116451cfc1
Rhino <mimiri3snsd0420@gmail.com>
2025-10-21 13:53:35 +0900

    [add] implement user authentication with Devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_21_034134) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_21_044917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_21_034134) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "encrypted_password"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "diaries", "playgrounds"
