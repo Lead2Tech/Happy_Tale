20de125667c4fa3aba26db0c6cceccfc16c183b2
Rhino <mimiri3snsd0420@gmail.com>
2025-10-24 17:44:44 +0900

    [23] Add Google Maps integration to playgrounds index page

--- db/schema.rb
+++ db/schema.rb
@@ -97,10 +97,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_24_034054) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "encrypted_password"
     t.string "nickname"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
